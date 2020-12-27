94088c3d71c1cbfc80ae09beef1e6e4e5efe08ae
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2020-12-27 20:29:10 +0200

    Update Askisi2.c

--- George Xanthos/Askisi2.c
+++ George Xanthos/Askisi2.c
@@ -3,14 +3,14 @@
 int main(int argc, char const *argv[]) {
   char c1;
   char c2;
-  printf("The grammar is :\n", );
+  printf("The grammar is :\n");
   printf("<E> --> (<Y>)\n");
   printf("<Y> --> <A><B>\n");
-  printf("<A> --> v|<E> \n", );
-  printf("<B> --> -<Y>|+<Y>|ε\n", );
+  printf("<A> --> v|<E> \n");
+  printf("<B> --> -<Y>|+<Y>|ε\n");
   printf("\n");
   printf("\n");
-  printf("So we begin with the E \n", );
+  printf("So we begin with the E \n");
   printf("<E> --> (<Y>)\n");
   printf("<Y> --> <A><B>\n");
   printf("Choose for A : you can chose : ν| <Ε>\n");
