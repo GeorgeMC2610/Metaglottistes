f28dad7ebff27b08a114f0135035e87abc784598
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2020-12-28 22:52:49 +0200

    Update Askisi.cpp

--- George Xanthos/Askisi.cpp
+++ George Xanthos/Askisi.cpp
@@ -2,18 +2,22 @@
 #include <string>
 using namespace std;
 int main(int argc, char const *argv[]) {
+  bool start = true;
+string answer ;
+string str;
+string choice;
+
+
 cout << "The rules of our grammar are :" << endl;
 cout << "E --> Y" << endl;
 cout << "Y --> AB" << endl;
-cout << "A -->  n or  empty" << endl;
+cout << "A -->  n or  <E> " << endl;
 cout << "B--> -Y or +Y or empty" << endl;
-bool start = true;
-string answer ;
-string str;
+
 while (start) {
   cout << "Do you want to start ???" << endl;
-  std::cin >> answer;
-  if (answer.compare("Yes")) {
+  cin >> answer;
+  if (answer.compare("Yes")==0) {
     start = true;
   } else {
     start = false;
@@ -23,6 +27,15 @@ while (start) {
     cout << str << endl;
     cout << "But <E> --> <Y>" << endl;
     cout << "str --> <Y>" << endl;
+    cout << "But also <Y> --> <A><B>"<< endl;
+    str =  "AB";
+    cout << "Your string is :  " << str << endl;  
+    cout << "At this point you have to make a choice : " << endl;
+    do  {
+    cin >> choice;
+    }
+    while (choice.compare("n")==0 || choice.compare("<E>")==0);
+
     
   }
 }
