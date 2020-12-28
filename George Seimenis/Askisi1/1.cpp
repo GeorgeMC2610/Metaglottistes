7702670177d87bc3fd641bc7ccd3a066ef930203
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-12-28 17:29:15 +0200

    το πρόγραμμα πλέον τρέχει σε μία μεγάλη λούπα. Δέχεται και 'stop'

--- George Seimenis/Askisi1/1.cpp
+++ George Seimenis/Askisi1/1.cpp
@@ -8,15 +8,36 @@ using namespace std;
 int main()
 {
 	//stack NAS;
+
+	cout << "Exercise 1 in 'Metaglottistes.'" << endl;
+	cout << "As soon as you type 'stop', the program will exit." << endl << endl;
 	string input;
 
-	cout << "Input: ";
-	cin >> input;
+	do 
+	{
+		cout << "Input: ";
+		cin >> input;
 
-	if (regex_match(input, regex("[xy]+")))
-		cout << "Correct";
-	else
-		cout << "Nope";
+		//this means "convert to lowercase"
+		transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return tolower(c); });
+
+		if (regex_match(input, regex("[xy]+")))
+		{
+			cout << "Correct" << endl;
+		}
+		else if (input.compare("stop") == 0)
+		{
+			break;
+		}	
+		else
+		{
+			cout << "Incorrect Input. Only x or y characters are allowed." << endl;
+		}
+			
+	} while(true);
+	
+	cout << "Program stopped" << endl;
+
 
 	return 0;
 }
