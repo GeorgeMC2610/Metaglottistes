6443f15350fc4838aae1084b3c2b2dcc872965b4
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-12-28 16:45:11 +0200

    Δοκιμές με regex

--- George Seimenis/Askisi1/1.cpp
+++ George Seimenis/Askisi1/1.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <stack>
+#include <string>
+#include <regex>
+
+using namespace std;
 
 int main()
 {
-	
-	
+	//stack NAS;
+	string input;
+
+	cout << "Input: ";
+	cin >> input;
+
+	if (regex_match(input, regex("[xy]+")))
+		cout << "Correct";
+	else
+		cout << "Nope";
+
 	return 0;
 }
