7fb4d5a21bd1db08a17fc3c36efee11d113e2019
GeorgeMC2610 <giorgosseimenis@gmail.com>
2020-12-28 20:40:56 +0200

    προσθήκη σχολίων στον κώδικα

--- George Seimenis/Askisi1/1.cpp
+++ George Seimenis/Askisi1/1.cpp
@@ -2,33 +2,43 @@
 #include <stack>
 #include <string>
 #include <regex>
+#include <typeinfo>
 
 using namespace std;
 
 int main()
 {
-	//stack NAS;
-
+	//initial message to be shown
 	cout << "Exercise 1 in 'Metaglottistes.'" << endl;
 	cout << "As soon as you type 'stop', the program will exit." << endl << endl;
+
 	string input;
 
+	//this do-while loop is going to run forever, unless the user types 'stop'.
 	do 
 	{
+		//get the input from the user.
 		cout << "Input: ";
 		cin >> input;
 
-		//this means "convert to lowercase"
+		//convert the input to lowercase.
 		transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return tolower(c); });
 
+		//if the message matches the wanted input (which has to be only x and y chars), we proceed to do what the exercise defines.
 		if (regex_match(input, regex("[xy]+")))
 		{
-			cout << "Correct" << endl;
+			//define the stack to be used, and the input length of the input (to use it in the for loop).
+			stack<char> NAS_stack;
+			int input_length = input.length();
+
+
 		}
+		//if the user wants to stop the program, break out of the loop, then exit.
 		else if (input.compare("stop") == 0)
 		{
 			break;
 		}	
+		//if the input doesn't match any case above, there must be some mistake. Inform the user accoordingly.
 		else
 		{
 			cout << "Incorrect Input. Only x or y characters are allowed." << endl;
@@ -36,8 +46,7 @@ int main()
 			
 	} while(true);
 	
+	//at this point, there's nothing else to be done. exit with code 0.
 	cout << "Program stopped" << endl;
-
-
 	return 0;
 }
