ea2789e4efec141c00f770bde386777c3ff30a61
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2021-01-02 16:49:04 +0200

    Προστέθηκαν συναρτήσεις αλλά πετάει out of range exception

--- George Xanthos/Thema.cpp
+++ George Xanthos/Thema.cpp
@@ -5,24 +5,28 @@
 using namespace std;
 
 
-void theEpsilon(string& str) {
-    //  int y = strlen(str);
-    cout << "<E> --> (<Y>)" << endl;
-    for (int i = 0; i < str.size(); i++) {
-        cout << str[i] << '\n';
+string theEpsilon(string str) {
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'E')
+        {
+            str[i] = 'Y';
+        }
     }
+    return str;
 }
 
-string theAlpha(string str) 
+string theAlpha(string str)
 {
+    cout << "We will replace the <A> according to the Rule <A> --> v| <E>" << endl;
     srand(time(NULL));
     int v1 = (rand() % 2);
 
     cout << v1 << endl;
 
-    if (v1 == 0) 
+    if (v1 == 0)
     {
-        for (int i = 0; i < str.length(); i++) 
+        for (int i = 0; i < str.length(); i++)
         {
             if (str[i] == 'A')
             {
@@ -30,9 +34,9 @@ string theAlpha(string str)
             }
         }
     }
-    else 
+    else
     {
-        for (int i = 0; i < str.length(); i++) 
+        for (int i = 0; i < str.length(); i++)
         {
             if (str[i] == 'A')
             {
@@ -43,12 +47,63 @@ string theAlpha(string str)
 
     return str;
 }
+string theBita(string str)
+{
+    srand(time(NULL));
+    int v1 = (rand() % 3);
+    for (int i = 0; i < str.length(); i++)
+    {
+        switch (v1)
+        {
+        case 0:
+            if (str[i] == 'B')
+            {
+                str[i] = '+Y';
+            }
+        case 1:
+            if (str[i] == 'B')
+            {
+                str[i] = '-Y';
+            }
+        case 2:
+            if (str[i] == 'B')
+            {
+                str[i] = 'E';
+            }
 
-int main() 
+
+        }
+    }
+
+    return str;
+}
+string theYpsilon(string str)
+{
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'Y')
+        {
+            str[i] = 'AB';
+        }
+    }
+    return str;
+}
+bool checkCapitals(string str) {
+    bool set = true;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (str[i] == 'A' || str[i] == 'E' || str[i] == 'B' || str[i] == 'Y')
+        {
+            set = false;
+        }
+    }
+    return set;
+}
+
+int main()
 {
     string str;
-    
-    // int v1 = rand() % 2;
+    bool set2;
     str = "E";
     cout << "<E> --> (<Y>)" << endl;
     str = "Y";
@@ -56,7 +111,33 @@ int main()
     cout << "<Y> --> <A><B> " << endl;
     str = "AB";
     cout << "The string becomes : " << str << endl;
+    for (int i = 1; i < 20; i++)
+    {
+        if (checkCapitals(str) == 0)
+        {
+            for (int y = 0; y < str.length(); y++)
+            {
+                switch (str[i])
+                {
 
-    str = theAlpha(str);
-    cout << str << endl;
+                case 'A':
+                    str = theAlpha(str);
+                    break;
+                case 'B':
+                    str = theBita(str);
+                    break;
+                case 'Y':
+                    str = theYpsilon(str);
+                    break;
+                case 'E':
+                    str = theEpsilon(str);
+                    break;
+                }
+
+
+
+            }
+        }
+    }
+
 }
