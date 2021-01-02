f1889891ae7569efa41d0188a8a4d6d00bb1c8ed
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2021-01-02 15:30:55 +0200

    Update Thema.cpp

--- George Xanthos/Thema.cpp
+++ George Xanthos/Thema.cpp
@@ -5,63 +5,59 @@
 using namespace std;
 
 
-
-
-void theEpsilon (string& str) {
-//  int y = strlen(str);
+void theEpsilon(string& str) {
+    //  int y = strlen(str);
     cout << "<E> --> (<Y>)" << endl;
-    for (int i  = 0; i < str.size(); i++) {
-      cout << str[i] << '\n';
+    for (int i = 0; i < str.size(); i++) {
+        cout << str[i] << '\n';
     }
 }
-void theAlpha (string& str, int v1 ) {
-      //  cout << v1 << endl;
-        if (v1==1) {
-            for (int i =0; i<str.size(); i++) {
-                int y = str.compare("A");
-                if (y==0) {
-                    str.replace(i,"A","v");
-                }
-            }
-        } else  {
-            for (int i =0; i<str.size(); i++) {
+void theAlpha(string &str) {
+            int v1 = rand() % 2;
+      cout << v1 << endl;
+    if (v1 == 0) {
+        for (int i = 0; i < str.length(); i++) {
             int y = str.compare("A");
-                if (y==0) {
-                    str.replace(i,"A","E");
-
-                }
+            if (y == 0) {
+                str[i] = 'v';
             }
         }
+    }
+    else {
+        for (int i = 0; i < str.length(); i++) {
+            int y = str.compare("A");
+            if (y == 0) {
+                str[i] = 'E';
+
+            }
+        }
+    }
+
+    cout << str << endl;
 
 
 
 
-          cout << str << endl;
-
-
-}
-
-
-
-
-int main () {
-string str;
-srand((unsigned) time(0));
-int v1  = 1 + (rand() % 3);
-//cout << v1 << endl;
-str = "E";
-cout << "<E> --> (<Y>)" << endl;
-str = "Y";
-cout << " The string is : " << str << endl ;
-cout << "<Y> --> <A><B> " <<endl;
-str = "ΑB";
-cout << "The string becomes : " << str << endl;
-//theEpsilon(str);
-theAlpha(str,v1);
-cout << str << endl;
-/*for (int counnter =2; counter <21; counter++ ) {
 
 }
-*/
+
+
+
+
+int main() {
+    string str;
+    srand((unsigned)time(0));
+   // int v1 = rand() % 2;
+    str = "E";
+    cout << "<E> --> (<Y>)" << endl;
+    str = "Y";
+    cout << " The string is : " << str << endl;
+    cout << "<Y> --> <A><B> " << endl;
+    str = "AB";
+    cout << "The string becomes : " << str << endl;
+
+    theAlpha(str);
+    cout << str << endl;
+
 
 }
