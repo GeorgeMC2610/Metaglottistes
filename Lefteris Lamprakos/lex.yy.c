0074f500fd9c96ad170c7df0f5aff5622389b00d
Left123456 <lefterislamprakos36@gmail.com>
2021-01-07 16:27:15 +0200

    found something interesting which i might not even use,lol
    kanonika to programma tha prepei na bgazei "lathos" an o xrhsths dwsei polla A px,mporw na to kanw na ta bgazei an k den me ti metaglhth a p uparxei an k den tha xreiastei gnka

--- Lefteris Lamprakos/lex.yy.c
+++ Lefteris Lamprakos/lex.yy.c
@@ -282,12 +282,12 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[12] =
+#define YY_NUM_RULES 5
+#define YY_END_OF_BUFFER 6
+static yyconst short int yy_accept[14] =
     {   0,
-        0,    0,    5,    3,    4,    2,    3,    0,    0,    1,
-        0
+        0,    0,    6,    4,    5,    2,    3,    2,    4,    0,
+        0,    1,    0
     } ;
 
 static yyconst int yy_ec[256] =
@@ -298,13 +298,13 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    3,    4,    4,    4,    4,    4,
+        4,    4,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    4,    1,    1,    5,
+        1,    1,    1,    1,    1,    1,    5,    1,    1,    6,
 
-        1,    1,    6,    1,    1,    1,    1,    1,    1,    1,
-        7,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    7,    1,    1,    1,    1,    1,    1,    1,
+        8,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -322,33 +322,35 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[8] =
+static yyconst int yy_meta[9] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1
+        1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static yyconst short int yy_base[12] =
+static yyconst short int yy_base[14] =
     {   0,
-        0,    0,   11,   12,   12,   12,    3,    2,    3,   12,
-       12
+        0,    0,   12,   13,   13,   13,   13,   13,    3,    2,
+        3,   13,   13
     } ;
 
-static yyconst short int yy_def[12] =
+static yyconst short int yy_def[14] =
     {   0,
-       11,    1,   11,   11,   11,   11,   11,   11,   11,   11,
-        0
+       13,    1,   13,   13,   13,   13,   13,   13,   13,   13,
+       13,   13,    0
     } ;
 
-static yyconst short int yy_nxt[20] =
+static yyconst short int yy_nxt[22] =
     {   0,
-        4,    5,    6,    6,    4,    7,    4,   10,    9,    8,
-       11,    3,   11,   11,   11,   11,   11,   11,   11
+        4,    5,    6,    7,    8,    4,    9,    4,   12,   11,
+       10,   13,    3,   13,   13,   13,   13,   13,   13,   13,
+       13
     } ;
 
-static yyconst short int yy_chk[20] =
+static yyconst short int yy_chk[22] =
     {   0,
-        1,    1,    1,    1,    1,    1,    1,    9,    8,    7,
-        3,   11,   11,   11,   11,   11,   11,   11,   11
+        1,    1,    1,    1,    1,    1,    1,    1,   11,   10,
+        9,    3,   13,   13,   13,   13,   13,   13,   13,   13,
+       13
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -367,7 +369,8 @@ char *yytext;
 #line 2 "Thema5.l"
     int a=0;
     int count=0;
-#line 371 "lex.yy.c"
+    int count1=0;
+#line 374 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -518,9 +521,9 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 5 "Thema5.l"
+#line 6 "Thema5.l"
 
-#line 524 "lex.yy.c"
+#line 527 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -571,13 +574,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 12 )
+				if ( yy_current_state >= 14 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 12 );
+		while ( yy_base[yy_current_state] != 13 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -605,25 +608,30 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "Thema5.l"
+#line 7 "Thema5.l"
 {++count;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 7 "Thema5.l"
+#line 8 "Thema5.l"
 {++a;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 8 "Thema5.l"
-
+#line 9 "Thema5.l"
+{++count1;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
-#line 9 "Thema5.l"
+#line 10 "Thema5.l"
+
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 11 "Thema5.l"
 ECHO;
 	YY_BREAK
-#line 627 "lex.yy.c"
+#line 635 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -915,7 +923,7 @@ static yy_state_type yy_get_previous_state()
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 12 )
+			if ( yy_current_state >= 14 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -950,11 +958,11 @@ yy_state_type yy_current_state;
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 12 )
+		if ( yy_current_state >= 14 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 11);
+	yy_is_jam = (yy_current_state == 13);
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1509,13 +1517,13 @@ int main()
 	return 0;
 	}
 #endif
-#line 9 "Thema5.l"
+#line 11 "Thema5.l"
 
 main() {
     printf("SKARNERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR\n");
     yylex();
 
-if (count == 1)
+if (count == 3)
 {
 printf("SKARNER SKARNER \n");
 }
@@ -1525,5 +1533,6 @@ printf("SKARNER \n");
 }
 
 printf("%d\n",count);
+printf("%d\n",count1);
 printf("%d\n",a);
 }
