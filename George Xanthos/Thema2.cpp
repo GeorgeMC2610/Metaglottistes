7bfd09053f8633756eed13265c0d5c5c2f5cafbd
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2021-01-08 20:59:08 +0200

    Βελτιοποίηση
    Μια αρκετά πιο λειτουργική επίλυση η οποία όμως έχει πολύ τυχαιότητα

--- George Xanthos/Thema2.cpp
+++ George Xanthos/Thema2.cpp
@@ -8,55 +8,78 @@ using namespace std;
 
 
  int main() {
-  string str = "AB";
-  int counter =0;
+  string str = "<A><B>";
+  string s1,s2;
+ // int counter =0;
+   int i=0;
   srand(time(NULL));
   int v1 = (rand() % 3);
   int v2 =(rand()% 2);
   cout << v1 << endl;
   cout << v2 << endl;
-  do {
-    for (int i =0 ;i<str.length();i++)
-    {
-      if (str[i]=='A')
-      {
-        if (v2 ==0)
-        {
-          str[i] = 'v';
+ 
+ for (int i=0; i<20; i++)
+ {
+    if (str[i]=='E')
+   {
+     s1 = str.substr(0,i);
+     s2 = str.substr(i+1,str.length());
+    str = s1 + "<" + "(" + "Y" + ")" + ">" + s2;
+    continue;
+   }
+   if (str[i]=='A' && v2 ==0)
+   {
+     str[i] = 'v';
+     continue;
+   }
+     if (str[i]=='A' && v2 ==1)
+   {
+     s1 = str.substr(0,i);
+     s2 = str.substr(i+1,str.length());
+     str = s1 + "<" + "E" + ">" + s2;
+     continue;
+   }
+     if (str[i]=='B' && v1 == 0 )
+   {
+     s1 = str.substr(0,i);
+      s2 = str.substr(i+1,str.length());
+     str = s1 +"+" +"<" + "Y" +">"+ s2;
+      continue;
+     
+   }
+    if ( str[i]=='B' && v1 ==1 )
+   {
+      s1 = str.substr(0,i);
+      s2 = str.substr(i+1 , str.length());
+     str = s1 + "-" +"<"+ "Y" +">"+ s2 ;
+      continue;
+     
+   }
+    if (str[i]=='B' && v1 ==2)
+   {
+      s1 = str.substr(0,i);
+      s2 = str.substr(i+1,str.length());
+     str = s1 + s2 ;
+      continue;
+     
+   }
+    if (str[i]=='Y')
+   {
+      s1 = str.substr(0,i);
+      s2 = str.substr(i+1,str.length());
+     str = s1 + "A" + "B" + s2;
+      continue;
+     
+   }
+ }
+ 
+  
+   
 
-        }else
-        {
-          str[i] = 'E';
-        }
-      }
-      if (str[i] == 'B')
-      {
-        if (v1 == 0 )
-        {
-          string s1 = str.substr(0,i-1);
-            string s2 = str.substr(i+1,str.length());
-            str = s1 + "+" + "Y" + s2;
-        }else if (v1 == 1)
-        {
-          string s1 = str.substr(0,i-1);
-            string s2 = str.substr(i+1,str.length());
-          str = s1 + "-" + "Y" +s2;
 
-        }else
-        {string s1 = str.substr(0,i-1);
-          string s2 = str.substr(i+1,str.length());
-          str = s1 +s2 ;
-
-        }
-        if (str[i] == 'Y')
-        {string s1 = str.substr(0,i-1);
-          string s2 = str.substr(i+1,str.length());
-          str = s1 +"A"+ "B"+ s2;
-        }
-      }
-    }
-  counter ++;
-  } while(counter<20);
+ 
+ 
+ 
 cout << str ;
   return 0;
 }
