8b5e64859980085c4ab484d1ca4e2d5aafcbc861
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-01-16 23:53:37 +0200

    now steps in thema2 have all the symbols
    menoun mono oi analutikes eksigiseis twn vhmatvn.Oi epanalhpseis einai 50

--- George Xanthos/Test.cpp
+++ George Xanthos/Test.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <regex>
 
 using namespace std;
 
@@ -17,50 +16,50 @@ public:
 		this->Expression = Expression;
 	}
 
-	static string GenerateExpression()
-	{
-		string str = "";
-		srand(time(NULL));
-
-		//to megethos tou string
-		int stringLength = 4 + (rand() % 4);
-
-		for (int i = 0; i < stringLength; i++)
+	/*	static string GenerateExpression()
 		{
-			int random = (rand() % 4);
+			string str = "";
+			//srand(time(NULL));
 
-			switch (random)
+			//to megethos tou string
+			int stringLength = 4 + (rand() % 4);
+
+			for (int i = 0; i < stringLength; i++)
 			{
-			case 0:
-				str += "E";
-				break;
-			case 1:
-				str += "Y";
-				break;
-			case 2:
-				str += "A";
-				break;
-			case 3:
-				str += "B";
-				break;
-			}
-		}
+				int random = (rand() % 4);
 
-		return str;
-	}
+				switch (random)
+				{
+				case 0:
+					str += "E";
+					break;
+				case 1:
+					str += "Y";
+					break;
+				case 2:
+					str += "A";
+					break;
+				case 3:
+					str += "B";
+					break;
+				}
+			}
+
+			return str;
+		}*/
 	bool Checker(string s)
 	{
 		//return regex_match(s,regex("[+-ne]+"));
-		string s2 = "";
+		//string s2 = "";
 		for (int i = 0; i < s.length(); i++)
 		{
-			s2 = s[i];
-			if (!regex_match(s2, regex("[+-ne]")))
+			//s2 = s[i];
+			if (s[i] == '<')
 			{
-				return false;
+				return true; //true ama vrei ena mh termatiko
 			}
 		}
-		return true;
+		return false; //false ama einai ola termatika
 
 	}
 
@@ -70,31 +69,42 @@ public:
 		int random;
 		//int i = 0;
 		int j = 0;
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 50; i++) {
 			j = 0;
 			if (!Checker(Expression))
 			{
 				break;
 			}
-			while (Expression[j] == 'n' || Expression[j] == '-' || Expression[j] == '+' || Expression[j] == 'e')
+			while (Expression[j] != '<')
 			{
 				j++;
 			}
-			switch (this->Expression[j])
+			switch (this->Expression[j + 1])
 			{
 			case 'E':
-				this->Expression[j] = 'Y';
+				this->Expression[j] = '(';
+				this->Expression[j + 1] = '<';
+				this->Expression[j + 2] = 'Y';
+				this->Expression.insert(j + 3, ">)");
 				break;
 			case 'Y':
-				this->Expression[j] = 'A';
-				this->Expression.insert(j + 1, "B");
+				this->Expression[j + 1] = 'A';
+				this->Expression.insert(j + 3, "<B>");
 				j++;
 				//i--;
 				break;
 			case 'A':
 				random = (rand() % 2);
 
-				this->Expression[j] = (random == 0) ? 'n' : 'E';
+				//this->Expression[j] = (random == 0) ? 'n' : 'E';
+
+				if (random == 0) {
+					this->Expression[j] = 'n';
+					this->Expression.erase(j + 1, 2);
+				}
+				else {
+					this->Expression[j + 1] = 'E';
+				}
 				break;
 			case 'B':
 				random = (rand() % 3);
@@ -102,27 +112,28 @@ public:
 				if (random == 0)
 				{
 					this->Expression[j] = '+';
-					this->Expression.insert(j + 1, "Y");
+					this->Expression[j + 1] = '<';
+					this->Expression[j + 2] = 'Y';
+					this->Expression.insert(j + 3, ">");
 					j++;
 				}
 				else if (random == 1)
 				{
 					this->Expression[j] = '-';
-					this->Expression.insert(j + 1, "Y");
+					this->Expression[j + 1] = '<';
+					this->Expression[j + 2] = 'Y';
+					this->Expression.insert(j + 3, ">");
 					j++;
 				}
-				else
-					this->Expression[j] = 'e';
+				else {
+					this->Expression.erase(j, 3);
+				}
 
 				//i--;
 				break;
 			}
-
 			//i++;
 			cout << Expression << endl;
-
-
-
 		}
 	}
 };
@@ -130,15 +141,14 @@ public:
 int main()
 {
 	srand(time(NULL));
-	Symbol testSymbol("E", false);
+	Symbol testSymbol("<E>", false);
+	string s = "abcfdbfhtrhrthbrdev";
+	s.erase(2, 1);
+	cout << s << endl;
 
 	cout << testSymbol.Expression << endl;
 
 	testSymbol.ReplaceCharacters();
 
-	cout << testSymbol.Expression << endl;
-
-
-
-
+	//cout << testSymbol.Expression << endl;
 }
