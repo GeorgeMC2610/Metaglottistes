80557b36206a4ca68a001ea42b930aea948b3eaf
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-01-16 19:54:21 +0200

    κομμίτ για τον Γιώργο

--- George Xanthos/Test.cpp
+++ George Xanthos/Test.cpp
@@ -1,90 +1,120 @@
-# include <iostream>
-# include <string>
-# include <ctime>
+#include <iostream>
+#include <string>
+#include <ctime>
 
 using namespace std;
 
-int  Random (int i)
+class Symbol
 {
-    srand((unsigned) time(0));
-    int result = 1 + (rand() % i);
-    return result;
-}
-string replace (string s)
-{
-    string str = s;
-    for (int i=0; i<s.length() ; i++)
-    { 
-        if (str[i]== 'A')
-        {
-            if (Random(2)==1)
-            {
-                str[i] = 'v';
-                break;
-            }
-            else if (Random(2)==2)
-            {
-                str[i] = 'E';
-                break;
-            }
-        } else if (str[i] == 'B')
-        {
-            if (Random(2) == 1)
-            {
-                str[i] = 'Y';
-                break;
-            } else if (Random(2) == 2)
-            {
-                str.erase(i);
-                break;
-            }
-        }
-        else if  (str[i] == 'E')
-        {
-            str [i] = 'Y';
-            break;
-        }
-        
-    }
-    return str;
-}
+public:
+	bool Final;
+	string Expression;
 
-bool check (string s)
-{
-    for (int i=0; i< s.length() ; i++)
-    {
-        if (s[i]== 'E'|| s[i] == 'Y'|| s[i]=='A' || s[i]=='B')
-        {
-            return true;
-        }
-        else 
-        {
-            return false;
-        }
-        
-    }
-}
+	Symbol(string Expression, bool Final)
+	{
+		this->Final = Final;
+		this->Expression = Expression;
+	}
+
+	static string GenerateExpression()
+	{
+		string str = "";
+		srand(time(NULL));
+
+		//to megethos tou string
+		int stringLength = 4 + (rand() % 4);
+
+		for (int i = 0; i < stringLength; i++)
+		{
+			int random = (rand() % 4);
+
+			switch (random)
+			{
+			case 0:
+				str += "E";
+				break;
+			case 1:
+				str += "Y";
+				break;
+			case 2:
+				str += "A";
+				break;
+			case 3:
+				str += "B";
+				break;
+			}
+		}
+
+		return str;
+	}
+
+	void ReplaceCharacters()
+	{
+		srand(time(NULL));
+		//<E>:: = (<Y>)
+		//<Y>:: = <A><B> *
+		//<A>:: = v | <E>
+		//<B>:: = -<Y> | +<Y> | e
+		int random;
+		int i = 0;
+		while (i < this->Expression.length())
+		{
+			switch (this->Expression[i])
+			{
+			case 'E':
+				this->Expression[i] = 'Y';
+				break;
+			case 'Y':
+				this->Expression[i] = 'A';
+				this->Expression.insert(i+1, "B");
+				i--;
+				break;
+			case 'A':
+				random = (rand() % 2);
+
+				this->Expression[i] = (random == 0) ? 'n' : 'E';
+				break;
+			case 'B':
+				random = (rand() % 3);
+
+				if (random == 0)
+				{
+					this->Expression[i] = '+';
+					this->Expression.insert(i + 1, "Y");
+				}
+				else if (random == 1)
+				{
+					this->Expression[i] = '-';
+					this->Expression.insert(i + 1, "Y");
+				}
+				else
+					this->Expression[i] = 'e';
+
+				i--;
+				break;
+			}
+
+			i++;
+		}
+	}
+};
 
 int main()
 {
-   
-  string str;
-  str = "AB";
- int counter = 0;
- while (check(str))
- {
+	Symbol testSymbol("", false);
+	testSymbol.Expression = Symbol::GenerateExpression();
+	cout << testSymbol.Expression << endl;
 
-     str = replace(str);
-    counter ++;
-    if (counter ==20)
-    {
-        break;
-    }
- }
- cout << str << endl;
- 
+	testSymbol.ReplaceCharacters();
+	cout << testSymbol.Expression << endl;
+	
 
+	//string s1 = "ABCDEF";
+	//s1.insert(3, "09");
+	//cout << s1 << endl;
 
-    return 0;
+	//s1.erase(2, 1);
+	//cout << s1;
 
+	//return 0;
 }
