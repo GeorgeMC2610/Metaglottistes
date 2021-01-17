0762cf37fe1e18334eb1bf3544b45ea3a11ef118
xanthosgeorge <56501711+xanthosgeorge@users.noreply.github.com>
2021-01-17 19:24:01 +0200

    Αλλαγές 2
    Πρστέθηκαν τα μηνύματα που ανέφερα στο προηγούμενο commit  , επίσης το πρόγραμματα έχει νομίζω επαρκή σχολιασμό  , θα γίνει  όμως και έγρραφο word ως technical  report

--- George Xanthos/Test.cpp
+++ George Xanthos/Test.cpp
@@ -7,22 +7,22 @@ using namespace std;
 class Symbol
 {
 public:
-	
+
 	string Expression;
 
 	Symbol(string Expression)
 	{
-		
-		Expression = Expression;
+
+		this->Expression = Expression;
 	}
 
-	
+
 	bool Checker(string s)
 	{
 		/*THe checker function scans from the left to the right to find the most left symbol.*/
 		for (int i = 0; i < s.length(); i++)
 		{
-			
+
 			if (s[i] == '<')
 			{
 				return true; //return true true if it finds it
@@ -33,8 +33,8 @@ public:
 	}
 
 	void ReplaceCharacters()
-	{ /*This function replace the most left symbol in our string with an other final or 
-	  non-final according to the grammatical rules we were given if checker function 
+	{ /*This function replace the most left symbol in our string with an other final or
+	  non-final according to the grammaticall rules we were given if checker function
 	  has returned true*/
 		int random;
 		int j;
@@ -42,9 +42,9 @@ public:
 			j = 0;
 			if (!Checker(Expression))
 			{
-				break; 
+				break;
 				/*
-				* if the checker returns false it means we have a final symbol 
+				* if the checker returns false it means we have a final symbol
 				* as the most left position so we transfer to the next most left position
 				* with a non-final symbol
 				*/
@@ -56,17 +56,20 @@ public:
 			switch (Expression[j + 1])
 			{
 			case 'E':
+				cout << "According to the grammatical rule :  <E> ::= (<Y>) " << endl;
 				Expression[j] = '(';
 				Expression[j + 1] = '<';
 				Expression[j + 2] = 'Y';
 				Expression.insert(j + 3, ">)");
 				break;
 			case 'Y':
+				cout << "According to the grammatical rule :  <Y> ::= <A><B> " << endl;
 				Expression[j + 1] = 'A';
 				Expression.insert(j + 3, "<B>");
 				j++;
 				break;
 			case 'A':
+				cout << "According to the grammatical rule :  <A> ::= v | <E> " << endl;
 				random = (rand() % 2);
 				if (random == 0) {
 					Expression[j] = 'n';
@@ -77,6 +80,7 @@ public:
 				}
 				break;
 			case 'B':
+				cout << "According to the grammatical rule :  <B> ::= -<Y> | +<Y> | empty space   " << endl;
 				random = (rand() % 3);
 				if (random == 0)
 				{
@@ -95,19 +99,19 @@ public:
 					j++;
 				}
 				/*
-				* According to the grammaticals rules  we have  <A> --> v|<E>
-				* and <B> --> -<Y>|+<Y> | empty space 
-				* so  in this case we should one symbol be chance ,
+				* According to the grammaticalls rules  we have  <A> --> v|<E>
+				* and <B> --> -<Y>|+<Y> | empty space
+				* so  in this case we should choose one symbol be chance ,
 				* in this case we use the rand() function in order to choose a symbol
 				* for the <A> and <B>
- 				* 
+				*
 				*/
 				else {
 					Expression.erase(j, 3);
 				}
 				break;
 			}
-			cout << Expression << endl;		
+			cout << Expression << endl;
 			/*
 			we give 50 steps for the replace to make a string that contains only non-final symbols if this is don't happen
 			 it will leave the nono-final symbols as they are
@@ -120,6 +124,6 @@ int main()
 {
 	srand(time(NULL)); // Giving a the time of the cpu as a ceed to our rand () function
 	Symbol testSymbol("<E>"); // We begin with <E> according to our rules so the Symbol() constructor takes <E> as an overload
-	//cout << testSymbol.Expression << endl; 
-	testSymbol.ReplaceCharacters(); 
+	cout << testSymbol.Expression << endl;
+	testSymbol.ReplaceCharacters();
 }
